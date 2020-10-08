8503da527c2f300942dff6fdae79a91bd7bbade5
Atunje <atunje_afolabi@yahoo.com>
2020-10-08 12:10:14 +0100

    Simple Service Container

---
+++ Profile.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+class Profile {
+    public function deactivateProfile(Setting $setting)
+    {
+
+    }
+}
---
+++ Setting.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App;
+
+class Setting
+{
+    public $isActive;
+}
